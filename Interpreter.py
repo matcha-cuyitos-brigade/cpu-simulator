fa161d750c4ab9777c7ea3c46fc75c7326e00de0
armi3 <fernandagonzalez@ufm.edu>
2019-08-24 15:30:37 -0600

    IR get method.

--- Interpreter.py
+++ Interpreter.py
@@ -1,3 +1,5 @@
+import re
+
 
 @staticmethod
 def get_assembly_line(instruction_address_register):
@@ -8,3 +10,42 @@ def get_assembly_line(instruction_address_register):
     list_filtered = [x for x in list_of_lines if "#" not in x]
     print(list_filtered)
     return list_filtered[instruction_address_register]
+
+
+@staticmethod
+def get_instruction_register(assembly_line):
+    if re.search("(OUTPUT)", assembly_line) or re.search("(0000)", assembly_line):
+        print("SÍ")
+    elif re.search("(LOAD_A)", assembly_line) or re.search("(0001)", assembly_line):
+        print("SÍ")
+    elif re.search("(LOAD_B)", assembly_line) or re.search("(0010)", assembly_line):
+        print("SÍ")
+    elif re.search("(AND)", assembly_line) or re.search("(0011)", assembly_line):
+        print("SÍ")
+    elif re.search("(ILD_A)", assembly_line) or re.search("(0100)", assembly_line):
+        print("SÍ")
+    elif re.search("(STORE_A)", assembly_line) or re.search("(0101)", assembly_line):
+        print("SÍ")
+    elif re.search("(STORE_B)", assembly_line) or re.search("(0110)", assembly_line):
+        print("SÍ")
+    elif re.search("(OR)", assembly_line) or re.search("(0111)", assembly_line):
+        print("SÍ")
+    elif re.search("(ILD_B)", assembly_line) or re.search("(1000)", assembly_line):
+        print("SÍ")
+    elif re.search("(ADD)", assembly_line) or re.search("(1001)", assembly_line):
+        print("SÍ")
+    elif re.search("(SUB)", assembly_line) or re.search("(1010)", assembly_line):
+        print("SÍ")
+    elif re.search("(JUMP)", assembly_line) or re.search("(1011)", assembly_line):
+        print("SÍ")
+    elif re.search("(JUMP_NEG)", assembly_line) or re.search("(1100)", assembly_line):
+        print("SÍ")
+    elif re.search("(LOAD_C)", assembly_line) or re.search("(1101)", assembly_line):
+        print("SÍ")
+    elif re.search("(STORE_D)", assembly_line) or re.search("(1110)", assembly_line):
+        print("SÍ")
+    elif re.search("(HALT)", assembly_line) or re.search("(1111)", assembly_line):
+        print("SÍ")
+
+
+get_instruction_register.__func__("OUTPUT A\n")
