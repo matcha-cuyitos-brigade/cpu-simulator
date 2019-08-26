f6350a1399fed6663419c4a75985c4c6a0d7cc74
armi3 <fernandagonzalez@ufm.edu>
2019-08-26 12:15:51 -0600

    Flask API server, first try.

--- Simulator.py
+++ Simulator.py
@@ -1,5 +1,19 @@
 from SoC import *
+from flask import Flask
+import config
+
+app = Flask(__name__)
 
 mysoc = SoC()
 print(mysoc.boot())
 mysoc.run()
+
+
+@app.route("/")
+def hello():
+    return "Hello World!"
+
+
+if __name__ == "__main__":
+    hello()
+    app.run(host="0.0.0.0", port=config.PORT, debug=config.DEBUG_MODE)
