d6b705130c6388e70758f70cb2bd0e62ab09613e
armi3 <fernandagonzalez@ufm.edu>
2019-08-26 18:09:20 -0600

    Docker build & run automatized.

--- start.sh
+++ start.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-app="docker.test"
-docker build -t ${app} .
-docker run -d -p 5001:80 \
-  --name=${app} \
-  -v "$PWD:/app" ${app}
+app="flask-api"
+docker build -t ${app}:latest .
+docker run -p 5000:80 ${app}
