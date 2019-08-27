9fd1c3e34bc33487c09dab37c706b2c58a3a7eba
armi3 <fernandagonzalez@ufm.edu>
2019-08-26 23:23:16 -0600

    API calls.

--- source/Simulator.py
+++ source/Simulator.py
@@ -1,5 +1,5 @@
 from SoC import *
-from flask import Flask, request #import main Flask class and request object
+from flask import Flask, request  # import main Flask class and request object
 from flask_cors import CORS
 import json
 
@@ -41,12 +41,48 @@ def boot_ui():
     x = json.dumps(boot_json)
     return x
 
-@app.route("/run_ui", methods=['GET', 'POST']) #allow both GET and POST requests
+
+@app.route("/run_ui", methods=['GET', 'POST'])  # allow both GET and POST requests
 def run_ui():
     if request.method == 'POST':  # this block is only entered when the form is submitted
+        code_selected = request.form.get('code_selected')
+
+    my_soc.run()
+    run_json = {
+        "register1": my_soc.cpu.register_a,
+        "register2": my_soc.cpu.register_b,
+        "register3": my_soc.cpu.register_c,
+        "register4": my_soc.cpu.register_d,
+        "RAM0": my_soc.ram.ram_array[0],
+        "RAM1": my_soc.ram.ram_array[1],
+        "RAM2": my_soc.ram.ram_array[2],
+        "RAM3": my_soc.ram.ram_array[3],
+        "RAM4": my_soc.ram.ram_array[4],
+        "RAM5": my_soc.ram.ram_array[5],
+        "RAM6": my_soc.ram.ram_array[6],
+        "RAM7": my_soc.ram.ram_array[7],
+        "RAM8": my_soc.ram.ram_array[8],
+        "RAM9": my_soc.ram.ram_array[9],
+        "RAM10": my_soc.ram.ram_array[10],
+        "RAM11": my_soc.ram.ram_array[11],
+        "RAM12": my_soc.ram.ram_array[12],
+        "RAM13": my_soc.ram.ram_array[13],
+        "RAM14": my_soc.ram.ram_array[14],
+        "RAM15": my_soc.ram.ram_array[15],
+        "input1": my_soc.cpu.alu.input_a,
+        "input2": my_soc.cpu.alu.input_b,
+        "output": my_soc.cpu.alu.output,
+        "flag_o": my_soc.cpu.alu.flag_overflow,
+        "flag_z": my_soc.cpu.alu.flag_zero,
+        "flag_n": my_soc.cpu.alu.flag_negative,
+        "ir": my_soc.cpu.cu.instruction_register,
+        "ira": my_soc.cpu.cu.instruction_address_register
+    }
+
+    x = json.dumps(run_json)
+    return x
 
 
 my_soc = SoC()
-my_soc.run()
 if __name__ == "__main__":
     app.run(host="0.0.0.0")
